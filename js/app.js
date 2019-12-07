c1371cf977f38699ba01510a565a328cfed59760
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-12-07 21:33:39 +0800

    feature: Add Charts section chart

--- js/app.js
+++ js/app.js
@@ -53,6 +53,7 @@ DATA
 ====================================================================== */
 // Traffic chart
 const trafficCanvas = document.querySelector('.traffic__chart');
+
 let trafficData = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
     "4-10", "11-17", "18-24", "25-31"
@@ -68,10 +69,10 @@ let trafficData = {
     pointBorderColor: 'rgba(116, 119, 191, 1)',
     pointBorderWidth: 2,
     pointRadius: 6,
-    pointHitRadius: 6,
     lineTension: 0
   }]
 };
+
 let trafficOptions = {
   aspectRatio: 2.5,
   animation: {
@@ -79,14 +80,82 @@ let trafficOptions = {
   },
   legend: {
     display: false
-  },
-
-
+  }
 };
-
 
 let trafficChart = new Chart(trafficCanvas, {
   type: 'line',
   data: trafficData,
   options: trafficOptions
+});
+
+
+
+// Daily Traffic Chart
+let dailyTrafficCanvas = document.querySelector('.charts__chart--daily');
+
+const dailyTrafficData = {
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1,
+    barThickness: 4,
+  }]
+};
+
+const dailyTrafficOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
+}
+
+let dailyTrafficChart = new Chart(dailyTrafficCanvas, {
+  type: 'bar',
+  data: dailyTrafficData,
+  options: dailyTrafficOptions
+});
+
+
+
+// Mobile Users Chart
+let mobileCanvas = document.querySelector('.charts__chart--mobile');
+
+const mobileData = {
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+      '#7477BF',
+      '#78CF82',
+      '#51B6C8'
+    ],
+    rotation: 90,
+  }]
+};
+
+const mobileOptions = {
+  legend: {
+    position: 'right',
+    labels: {
+      boxWidth: 20,
+      fontStyle: 'bold'
+    }
+  }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+  type: 'doughnut',
+  data: mobileData,
+  options: mobileOptions
 });
