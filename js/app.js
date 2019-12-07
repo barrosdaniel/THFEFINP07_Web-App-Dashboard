9ee18e1f55ebaeea6f9fb556f81a8a716572ccc5
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-12-07 19:49:42 +0800

    feature: Add alerts pop-down on click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 /* ======================================================================
 UI Elements
 ====================================================================== */
+const notificationsIcon = document.querySelector('.header__widgets--icon-container');
+const dropdownContent = document.querySelector('.header__widgets--dropdown-content');
 const alertSection = document.querySelector('.alert');
 const alertBox = document.querySelector('.alert-box');
 
@@ -8,18 +10,28 @@ const alertBox = document.querySelector('.alert-box');
 /* ======================================================================
 Event Listeners
 ====================================================================== */
+notificationsIcon.addEventListener('click', notificationsDropdownClick);
 alertBox.addEventListener('click', closeAlertBox);
 
 
 /* ======================================================================
 Global Controllers
 ====================================================================== */
+dropdownContent.style.display = 'none';
 
 
 
 /* ======================================================================
 Logic
 ====================================================================== */
+function notificationsDropdownClick() {
+  if (dropdownContent.style.display === 'none') {
+    dropdownContent.style.display = 'block';
+  } else if (dropdownContent.style.display === 'block') {
+    dropdownContent.style.display = 'none';
+  }
+}
+
 function closeAlertBox(e) {
   if (e.target.tagName === 'BUTTON' || e.target.tagName === 'I') {
     alertBox.style.display = 'none';
