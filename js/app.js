e58f87d236dede6ebe9dcd5f1193c32ce63f9be1
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-12-07 20:57:23 +0800

    feature: Add Traffic section chart

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
 UI Elements
 ====================================================================== */
 const notificationsIcon = document.querySelector('.header__widgets--icon-container');
+const newNotificationsMarker = document.querySelector('.header__widgets--notification');
 const dropdownContent = document.querySelector('.header__widgets--dropdown-content');
 const alertSection = document.querySelector('.alert');
 const alertBox = document.querySelector('.alert-box');
-
 
 /* ======================================================================
 Event Listeners
@@ -25,6 +25,7 @@ dropdownContent.style.display = 'none';
 Logic
 ====================================================================== */
 function notificationsDropdownClick() {
+  newNotificationsMarker.style.visibility = 'hidden';
   if (dropdownContent.style.display === 'none') {
     dropdownContent.style.display = 'block';
   } else if (dropdownContent.style.display === 'block') {
@@ -43,3 +44,49 @@ function closeAlertBox(e) {
 Testing
 ====================================================================== */
 // console.log(alertBox);
+
+
+
+
+/* ======================================================================
+DATA
+====================================================================== */
+// Traffic chart
+const trafficCanvas = document.querySelector('.traffic__chart');
+let trafficData = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+    "4-10", "11-17", "18-24", "25-31"
+  ],
+  datasets: [{
+    data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250,
+      1750
+    ],
+    backgroundColor: 'rgba(116, 119, 191, .3)',
+    borderWidth: 1,
+    borderColor: 'rgba(116, 119, 191, 1)',
+    pointBackgroundColor: 'rgba(249, 249, 249, 1)',
+    pointBorderColor: 'rgba(116, 119, 191, 1)',
+    pointBorderWidth: 2,
+    pointRadius: 6,
+    pointHitRadius: 6,
+    lineTension: 0
+  }]
+};
+let trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  legend: {
+    display: false
+  },
+
+
+};
+
+
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+});
