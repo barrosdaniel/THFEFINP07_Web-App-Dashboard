cf0912e8ef15220d4a7c0c55dfd25f53fd2f5316
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-12-10 20:46:16 +0800

    feature: Add Traffic Chart rendering for different periods

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const newNotificationsMarker = document.querySelector('.header__widgets--notific
 const dropdownContent = document.querySelector('.header__widgets--dropdown-content');
 const alertSection = document.querySelector('.alert');
 const alertBox = document.querySelector('.alert-box');
+const trafficButtonsList = document.querySelector('.traffic__menu');
+const trafficButtons = document.querySelectorAll('.traffic__button');
+const trafficButtonActive = document.querySelectorAll('.traffic__button--active');
 const userSearch = document.querySelector('.user__search');
 const userMessage = document.querySelector('.user__message');
 const userSendButton = document.querySelector('.user__button--send');
@@ -14,10 +17,11 @@ const userSendButton = document.querySelector('.user__button--send');
 /* ======================================================================
 Event Listeners
 ====================================================================== */
+body.addEventListener('click', notificationsDropdownHideOnClickOutside);
 notificationsIcon.addEventListener('click', notificationsDropdownClick);
+trafficButtonsList.addEventListener('click', ChangeTrafficChartPeriod);
 alertBox.addEventListener('click', closeAlertBox);
 userSendButton.addEventListener('click', sendUserMessage);
-body.addEventListener('click', notificationsDropdownHideOnClickOutside);
 
 
 /* ======================================================================
@@ -30,6 +34,7 @@ dropdownContent.style.display = 'none';
 /* ======================================================================
 Logic
 ====================================================================== */
+// Header
 function notificationsDropdownHideOnClickOutside(e) {
   if (dropdownContent.style.display === 'block' && e.target.parentElement.classList.value !== 'header__widgets--dropdown-content' && e.target.parentElement.classList.value !== 'header__widgets--icon-container') {
     dropdownContent.style.display = 'none';
@@ -52,6 +57,17 @@ function closeAlertBox(e) {
   }
 }
 
+// Traffic Chart Section
+function ChangeTrafficChartPeriod(e) {
+  if (e.target.classList.value.includes('traffic__button')) {
+    trafficChartPeriod = e.target.textContent.toLowerCase();
+    renderTrafficCanvas();
+  } else {
+    console.log(`Chart period not specified`);
+  }
+}
+
+// User Section
 function sendUserMessage(e) {
   e.preventDefault();
   if (userSearch.value === "" && userMessage.value === "") {
@@ -78,43 +94,83 @@ DATA
 ====================================================================== */
 // Traffic chart
 const trafficCanvas = document.querySelector('.traffic__chart');
+const hourlyLabels = ["00-06", "06-12", "12-18", "18-00"];
+const hourlyData = [750, 3380, 2750, 4800];
+const dailyLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"];
+const dailyData = [450, 350, 400, 425, 525, 1100, 980, 450, 350, 400, 425, 525, 1100, 980, 450, 350, 400, 425, 525, 1100, 980, 450, 350, 400, 425, 525, 1100, 980, 450, 350];
+const weeklyLabels = ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+  "4-10", "11-17", "18-24", "25-31"
+];
+const weeklyData = [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250,
+  1750
+];
+const monthlyLabels = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+const monthlyData = [26800, 25750, 25500, 26000, 23780, 21200, 16950, 17250, 17500, 19200, 21750, 24800, 25750];
 
-let trafficData = {
-  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
-    "4-10", "11-17", "18-24", "25-31"
-  ],
-  datasets: [{
-    data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250,
-      1750
-    ],
-    backgroundColor: 'rgba(116, 119, 191, .3)',
-    borderWidth: 1,
-    borderColor: 'rgba(116, 119, 191, 1)',
-    pointBackgroundColor: 'rgba(249, 249, 249, 1)',
-    pointBorderColor: 'rgba(116, 119, 191, 1)',
-    pointBorderWidth: 2,
-    pointRadius: 6,
-    lineTension: 0
-  }]
-};
+let trafficChartPeriod = 'weekly';
 
-let trafficOptions = {
-  aspectRatio: 2.5,
-  animation: {
-    duration: 0
-  },
-  legend: {
-    display: false
+function getTrafficChartData() {
+  const period = trafficChartPeriod;
+  console.log(period);
+  const chartData = [];
+  console.log(chartData);
+
+  if (period === 'hourly') {
+    chartData.push(hourlyLabels);
+    chartData.push(hourlyData);
+  } else if (period === 'daily') {
+    chartData.push(dailyLabels);
+    chartData.push(dailyData);
+  } else if (period === 'weekly') {
+    chartData.push(weeklyLabels);
+    chartData.push(weeklyData);
+  } else if (period === 'monthly') {
+    chartData.push(monthlyLabels);
+    chartData.push(monthlyData);
   }
-};
+  return chartData;
+}
 
-let trafficChart = new Chart(trafficCanvas, {
-  type: 'line',
-  data: trafficData,
-  options: trafficOptions
-});
+function renderTrafficCanvas() {
+  const chartData = getTrafficChartData();
+  console.log(chartData);
+  const labels = chartData[0];
+  const data = chartData[1];
 
+  let trafficData = {
+    labels: labels,
+    datasets: [{
+      data: data,
+      backgroundColor: 'rgba(116, 119, 191, .3)',
+      borderWidth: 1,
+      borderColor: 'rgba(116, 119, 191, 1)',
+      pointBackgroundColor: 'rgba(249, 249, 249, 1)',
+      pointBorderColor: 'rgba(116, 119, 191, 1)',
+      pointBorderWidth: 2,
+      pointRadius: 6,
+      lineTension: 0
+    }]
+  };
 
+  let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+      duration: 0
+    },
+    legend: {
+      display: false
+    }
+  };
+
+  let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+  });
+
+}
+
+renderTrafficCanvas();
 
 // Daily Traffic Chart
 let dailyTrafficCanvas = document.querySelector('.charts__chart--daily');
